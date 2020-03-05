47a723888b8592d2292a5be793be295fdfa874c3
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-03-04 19:04:06 -0600

    Funcion de Buscador

--- src/data.js
+++ src/data.js
@@ -1,13 +1,20 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
+//importa la data a trabajar
+import data from './data/rickandmorty/rickandmorty.js';
 
-// esta es una función de ejemplo
-
-export const example = () => {
-  return 'example';
+let chars = data.results
+//crea la funcion para buscar por medio de input   
+export const info = (e) => {
+    let word = e.target.value
+    let regEx = new RegExp(word, "i")
+    let result = chars.filter(char=>regEx.test(char.name))
+    let Resultados = "<h1>Personajes</h1>";
+    for (let i = 0; i < result.length; i++) {
+        Resultados+=" <h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
+        Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
+        Resultados+=" <p>Especie: "+ result[i].species+" </p>";
+        Resultados+=" <p>Genero: "+ result[i].gender+" </p>";
+    }
+    return Resultados;
+                 
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { example } from './data.js';
+//import { example } from './data.js';    
+//nodos
+let solucion = document.querySelector("#encabezado");
+let datos = document.querySelector("#datos");
+let buscador = document.querySelector("#Buscar");
 
-console.log(example);
-
+//importa la funcion del archivo data
+import { info } from './data.js';
+//listener
+document.querySelector('#buscador').addEventListener('change', function(e){
+    datos.innerHTML = info(e);
+}); 
+            
+    
