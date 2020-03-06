fd0c44381e7183734da612b430bb5bc8d9279ff8
ElizaFlores11 <60673917+ElizaFlores11@users.noreply.github.com>
2020-03-06 14:48:38 -0600

    Modificacion de Imagenes y Flexbox

--- src/data.js
+++ src/data.js
@@ -8,13 +8,17 @@ export const info = (e) => {
     let regEx = new RegExp(word, "i")
     let result = chars.filter(char=>regEx.test(char.name))
     let Resultados = " <h1>Personajes</h1>";
+    //Resultados+="<div style = 'direction: rtl; display: flex; justify-content: flex-end;'>"
+    Resultados+="<div class='acomodar'>"; 
     for (let i = 0; i < result.length; i++) {
-        Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+="<div> <h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
         Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
         Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
         Resultados+=" <p>Especie: "+ result[i].species+" </p>";
         Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
     }
+        Resultados+="</div>"; 
     return Resultados;
                  
 };
@@ -23,72 +27,99 @@ export const Genero = (genero) => {
     let g = genero; 
     let result = chars.filter(chars => chars.gender == g); 
     let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
     for (let i = 0; i < result.length; i++) {
         Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
         Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
         Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
         Resultados+=" <p>Especie: "+ result[i].species+" </p>";
         Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
     }
-
-    return Resultados;
-                 
+        Resultados+="</div>";
+    return Resultados;             
 };
 
+export const OtroGenero = () => {
+    let result = chars.filter(chars => chars.gender != "Female" && chars.gender != "Male" ); 
+    let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
+    for (let i = 0; i < result.length; i++) {
+        Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
+        Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
+        Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
+        Resultados+=" <p>Especie: "+ result[i].species+" </p>";
+        Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
+    }
+        Resultados+="</div>";
+    return Resultados;             
+};
 export const Status = (status) => {
     let s = status; 
     let result = chars.filter(chars => chars.status == s); 
     let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
     for (let i = 0; i < result.length; i++) {
         Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
         Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
         Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
         Resultados+=" <p>Especie: "+ result[i].species+" </p>";
         Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
  }
-
-    return Resultados;
-                 
-
-                 
+    Resultados+="</div>";
+    return Resultados;              
 };
 
+export const OtroStatus = () => {
+    let s = status; 
+    let result = chars.filter(chars => chars.status != "Alive" && chars.status != "Dead"); 
+    let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
+    for (let i = 0; i < result.length; i++) {
+        Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
+        Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
+        Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
+        Resultados+=" <p>Especie: "+ result[i].species+" </p>";
+        Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
+ }
+     Resultados+="</div>";
+    return Resultados;              
+};
 export const Especie = (especie) => {
     let e = especie; 
     let result = chars.filter(chars => chars.species == e); 
     let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
     for (let i = 0; i < result.length; i++) {
         Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
         Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
         Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
         Resultados+=" <p>Especie: "+ result[i].species+" </p>";
         Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
  }
-
-    return Resultados;
-                 
-
-                 
+     Resultados+="</div>";
+    return Resultados;                
 };
 
 export const location = (locacion) => {
     let word = locacion; 
     let regEx = new RegExp(word, "i")
-    //let result = chars.filter(chars=>regExp.test(chars.location))
-    let result = chars.filter(char=>regEx.test(char.location))
-    
-    //let result = chars.filter(chars => chars.species == e); 
-    /*let Resultados = " <h1>Personajes</h1>";
+    let result = chars.filter(chars => regEx.test(chars.location.name)) 
+    let Resultados = " <h1>Personajes</h1>";
+        Resultados+="<div class='acomodar'>"; 
     for (let i = 0; i < result.length; i++) {
         Resultados+=" <div><h2>" + result[i].name+" </h2>";; 
+        Resultados+=" <img src='"+ result[i].image +"'/>";
         Resultados+=" <p>Id:  "+ result[i].id+" </p>";;
         Resultados+=" <p>Estatus: "+ result[i].status+" </p>"; 
         Resultados+=" <p>Especie: "+ result[i].species+" </p>";
-        Resultados+=" <p>Genero: "+ result[i].gender+" </p></div>";
- }*/
-
-    return result;
-                 
-
-                 
+        Resultados+=" <p>Genero: "+ result[i].gender+" </p>"; 
+        Resultados+=" <p>Locacion: "+ result[i].location.name+" </p></div>";
+    }
+    Resultados+="</div>";
+    return Resultados;
 };
--- src/main.js
+++ src/main.js
@@ -7,34 +7,41 @@ let buscador = document.querySelector("#Buscar");
 //importa la funcion del archivo data
 import { info } from './data.js';
 import { Genero } from './data.js';
+import { OtroGenero } from './data.js';
 import { Status } from './data.js';
+import { OtroStatus } from './data.js';
 import { Especie } from './data.js';
 import { location } from './data.js';
 //listener
+//Buscador
 document.querySelector('#buscador').addEventListener('change', function(e){
     datos.innerHTML = info(e);
 });
+//Funciones de Genero
 document.querySelector('#femenino').addEventListener('click', function(){
     let gener = "Female"; 
-    //console.log(gener); 
     datos.innerHTML = Genero(gener);
-}); 
+});
 document.querySelector('#Masculino').addEventListener('click', function(){
     let gener = "Male"; 
     datos.innerHTML = Genero(gener);
-}); 
+});
+document.querySelector('#OtroGenero').addEventListener('click', function(){
+    datos.innerHTML = OtroGenero();
+});
+//Funciones de Status
 document.querySelector('#vivo').addEventListener('click', function(){
     let status = "Alive"; 
     datos.innerHTML = Status(status);
-}); 
+});
 document.querySelector('#muerto').addEventListener('click', function(){
     let status = "Dead"; 
     datos.innerHTML = Status(status);
 }); 
-document.querySelector('#Otro').addEventListener('click', function(){
-    let status = "unknown"; 
-    datos.innerHTML = Status(status);
-}); 
+document.querySelector('#OtroEstatus').addEventListener('click', function(){
+   datos.innerHTML = OtroStatus(); 
+});  
+//Funciones de Especies
 document.querySelector('#humanos').addEventListener('click', function(){
     let especie = "Human"; 
     datos.innerHTML = Especie(especie);
@@ -43,12 +50,15 @@ document.querySelector('#otro').addEventListener('click', function(){
     let especie = "Alien"; 
     datos.innerHTML = Especie(especie);
 }); 
+//Funciones de Locacion
 document.querySelector('#tierra').addEventListener('click', function(){
-    //let Locacion = "Earth (Replacement Dimension)l";
     let Locacion = "Earth"; 
     datos.innerHTML = location(Locacion);
-}); 
-
+});
+document.querySelector('#OtroLocacion').addEventListener('click', function(){
+    let Locacion = "Interdimensional"; 
+    datos.innerHTML = location(Locacion);
+});
 
             
     
